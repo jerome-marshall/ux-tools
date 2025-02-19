5cd2bb670430fc0ca37abeddbc0fb617f7498919
Jerome Marshall <jeromemarshall0@gmail.com>
2025-02-19 23:05:54 +0530

    chore: add global CSS import to root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import "../styles/global.css";
 
 export const Route = createRootRoute({
   component: () => (
