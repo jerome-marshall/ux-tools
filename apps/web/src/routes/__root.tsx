4e75e33d23a06e7838a70a67aaf611153426aba2
Jerome Marshall <jeromemarshall0@gmail.com>
2025-03-04 22:43:06 +0530

    feat: init monorepo

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import "../styles/global.css";
 import Header from "@/components/header";
+import { Toaster } from "@/components/ui/sonner";
 
 export const Route = createRootRoute({
   component: () => (
@@ -12,6 +13,7 @@ export const Route = createRootRoute({
           <Outlet />
         </div>
       </div>
+      <Toaster />
       <TanStackRouterDevtools />
     </>
   ),
