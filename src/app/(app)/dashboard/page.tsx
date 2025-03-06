3ff46e6156f3bf3175d8307fcd70680b68b0fe4b
Jerome Marshall <jeromemarshall0@gmail.com>
2025-03-06 15:04:18 +0530

    refactor: Reorganize project structure and update styling

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello Dashboard</h1>
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <h1 className='font-geist text-4xl font-bold'>Hello Dashboard</h1>
     </div>
-  );
+  )
 }
