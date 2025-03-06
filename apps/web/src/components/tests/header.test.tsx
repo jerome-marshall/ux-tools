bd994176cffffdcb0545af755f9dbf05ab51d81d
Jerome Marshall <jeromemarshall0@gmail.com>
2025-03-06 09:17:34 +0530

    DELETE MONOREPO

--- apps/web/src/components/tests/header.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { customRender, screen } from "@/tests/test-utils";
-import Header from "../header";
-
-test("renders header", async () => {
-  await customRender(<Header />);
-  expect(screen.getByText("Ux Lab")).toBeInTheDocument();
-});
