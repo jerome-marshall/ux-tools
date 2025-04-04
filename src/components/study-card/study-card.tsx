daa76719176f25dafeec0f5db2980d71faa18879
Jerome Marshall <jeromemarshall0@gmail.com>
2025-04-04 09:56:51 +0530

    feat: dynamic results count in study card

--- src/components/study-card/study-card.tsx
+++ src/components/study-card/study-card.tsx
@@ -3,6 +3,9 @@ import { format } from 'date-fns'
 import { FlaskConical } from 'lucide-react'
 import Link from '@/components/link'
 import { studyUrl } from '@/utils/urls'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { useTRPC } from '@/trpc/client'
+import { Suspense } from 'react'
 const StudyCard = ({ study }: { study: Study }) => {
   return (
     <Link
@@ -18,11 +21,34 @@ const StudyCard = ({ study }: { study: Study }) => {
       <div className='flex flex-col gap-1'>
         <p className='text-base font-medium'>{study.name}</p>
         <div className='flex items-center justify-between gap-2 text-xs text-gray-500'>
-          <p className=''>{2} reponses</p>
+          <Suspense fallback={<p className=''>Loading...</p>}>
+            <StudyResponseCount studyId={study.id} />
+          </Suspense>
           <p className=''>{format(study.createdAt, 'MMM d, yyyy')}</p>
         </div>
       </div>
     </Link>
+  )
+}
+
+const StudyResponseCount = ({ studyId }: { studyId: string }) => {
+  const trpc = useTRPC()
+
+  const {
+    data: { resultsData: testResults }
+  } = useSuspenseQuery(
+    trpc.tests.getTestResults.queryOptions({
+      studyId
+    })
+  )
+
+  const responseCount = testResults.reduce((acc, curr) => acc + curr.results.length, 0)
+  const responseCountText = responseCount === 1 ? 'response' : 'responses'
+
+  return (
+    <p className=''>
+      {responseCount} {responseCountText}
+    </p>
   )
 }
 
