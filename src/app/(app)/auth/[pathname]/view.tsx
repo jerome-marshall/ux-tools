bb7bae4dbe79083fa021293e88b87a64cf879e7f
Jerome Marshall <jeromemarshall0@gmail.com>
2025-04-14 17:22:40 +0530

    feat: enhance AuthCard styling with custom classNames

--- src/app/(app)/auth/[pathname]/view.tsx
+++ src/app/(app)/auth/[pathname]/view.tsx
@@ -14,7 +14,14 @@ export function AuthView({ pathname }: { pathname: string }) {
 
   return (
     <main className='flex grow flex-col items-center justify-center gap-4 p-4'>
-      <AuthCard pathname={pathname} />
+      <AuthCard
+        pathname={pathname}
+        classNames={{
+          settings: {
+            tabs: { list: 'bg-gray-200' }
+          }
+        }}
+      />
     </main>
   )
 }
